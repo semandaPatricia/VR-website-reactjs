a531fe7ca6271f90377ac29166469d6bfaa25716
semanda <patash248@gmail.com>
2024-03-22 16:04:26 +0100

    Add HeroSection component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 
-import './App.css'
+//import './App.css'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <Navbar/>
+    <>
+      <Navbar/>
+   <HeroSection/>
+    </>
+ 
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80 bg-white text-black">
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
@@ -36,14 +36,15 @@ const Navbar = () => {
               Create an account
             </a>
           </div>
-          <div className="lg:hidden md:flex flex-col justify-end">
+          {/*mobile devices */}
+          <div className="lg:hidden md:flex flex-col justify-end ">
             <button onClick={toggleNavbar}>
               {mobileDrawerOpen ? <X /> : <Menu />}
             </button>
           </div>
         </div>
         {mobileDrawerOpen && (
-          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+          <div className="fixed right-0 z-20 bg-neutral-900 text-white w-full p-12 flex flex-col justify-center items-center lg:hidden">
             <ul>
               {navItems.map((item, index) => (
                 <li key={index} className="py-4">
